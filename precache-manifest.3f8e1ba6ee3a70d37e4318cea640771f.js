86b3ddc717f0a5a59b9553624c7015ace8a41242
gruppjo <jona_blubb@web.de>
2019-07-22 13:33:42 +0200

    build[storybook]: July 22, 19 - 13:33

---
+++ precache-manifest.3f8e1ba6ee3a70d37e4318cea640771f.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657f83a078333305173675d5d678a064",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.58d877c8ebfb4e65d10e.bundle.js"
+  },
+  {
+    "revision": "d2f0f5b115d31cdb9c8d",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.58d877c8ebfb4e65d10e.bundle.js"
+  },
+  {
+    "revision": "2963dcbe5c3dc388cf606d71b1344af3",
+    "url": "static/media/logo.2963dcbe.png"
+  },
+  {
+    "url": "vendors~main.58d877c8ebfb4e65d10e.bundle.js"
+  }
+]);
