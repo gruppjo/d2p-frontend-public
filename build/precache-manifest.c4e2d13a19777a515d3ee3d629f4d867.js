55f81f9e880d174761b44b2508645492b47607c1
gruppjo <jona_blubb@web.de>
2019-07-23 18:05:20 +0200

    build[app]: July 23, 19 - 18:07

---
+++ build/precache-manifest.c4e2d13a19777a515d3ee3d629f4d867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d5621ee89c9b114fa259a0878509d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a03c6ddcfbb3b0c034a",
+    "url": "/static/css/main.fb37d145.chunk.css"
+  },
+  {
+    "revision": "8f6401ed88b101e397ad",
+    "url": "/static/js/2.bfdc7730.chunk.js"
+  },
+  {
+    "revision": "7a03c6ddcfbb3b0c034a",
+    "url": "/static/js/main.da56dd9c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
